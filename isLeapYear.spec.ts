52e03c669149b5c69758d1ac6916bfa88db9a4c5
DESKTOP-MRO5CVV\kaiti <kaitie.98@hotmail.com>
2021-09-11 13:42:01 +1000

    Add coverage and test script

--- isLeapYear.spec.ts
+++ isLeapYear.spec.ts
@@ -3,4 +3,5 @@ import { isLeapYear } from './isLeapYear';
 test('Check if a given year is a leap year', () => {
   expect(isLeapYear(2021)).toBe(false);
   expect(isLeapYear(2020)).toBe(true);
+  expect(isLeapYear(2000)).toBe(true);
 });
